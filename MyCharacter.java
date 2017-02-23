80bbb48f0399e94888acbe9442ca17d0c510284b
Tao <taoli1021@gmail.com>
2017-02-23 16:34:41 +0000

    original

---
+++ MyCharacter.java
@@ -0,0 +1,33 @@
+package assignment1;
+
+public class MyCharacter {
+	// Returns true if ch is a lowercase character ('a' to 'z'); false otherwise.
+	public static boolean isLowerCase(char ch) {
+		return false;
+	}
+
+	// Returns true if ch is an uppercase character ('A' to 'Z'); false otherwise.
+	public static boolean isUpperCase(char ch) {
+		return false;
+	}
+
+	// Returns true if ch is a digit ('0' to '9'); false otherwise.
+	public static boolean isDigit(char ch) {
+		return false;
+	}
+
+	// Returns true if ch is a space (' ') or tab ('\t') character; false otherwise.
+	public static boolean isWhitespace(char ch) {
+		return false;
+	}
+
+	// Returns the lowercase equivalent of ch, if any; otherwise, ch itself is returned.
+	public static char toLowerCase(char ch) {
+		return 0;
+	}
+
+	// Returns the uppercase equivalent of ch, if any; otherwise, ch itself is returned.
+	public static char toUpperCase(char ch) {
+		return 0;
+	}
+}
---
+++ MyString.java
@@ -0,0 +1,49 @@
+package assignment1;
+
+public class MyString {
+	// Returns a new string with the characters in str converted to lowercase.
+	public static String toLowerCase(String str) {
+		return "";
+	}
+
+	// Returns a new string with the characters in str converted to uppercase.
+	public static String toUpperCase(String str) {
+		return "";
+	}
+
+	// Returns true if the two strings s1 and s2 are equal; false otherwise. 
+	// Two strings are considered equal if they are of the same length and 
+	// corresponding characters in the two strings are equal.
+	public static boolean equals(String s1, String s2) {
+		return false;
+	}
+	
+	// Returns true if the two strings s1 and s2 are equal ignoring case; false 
+	// otherwise. Two strings are considered equal if they are of the same length 
+	// and corresponding characters in the two strings are equal ignoring case.
+	public static boolean equalsIgnoreCase(String s1, String s2) {
+		return false;
+	}
+
+	// Returns a new string with every occurrence of character ch1 in str replaced 
+	// with character ch2.	
+	public static String replace(String str, char ch1, char ch2) {
+		return "";
+	}
+
+	// Returns a new string with all leading and trailing whitespace characters (space 
+	// or tab characters) in str removed.
+	public static String trim(String str) {
+		return "";
+	}
+	
+	// Returns true if str starts with the specified prefix; false otherwise.
+	public static boolean startsWith(String str, String prefix) {
+		return false;
+	}
+
+	// Returns true if str ends with the specified suffix; false otherwise.
+	public static boolean endsWith(String str, String suffix) {
+		return false;
+	}
+}
